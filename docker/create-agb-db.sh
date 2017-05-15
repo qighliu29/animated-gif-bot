a4588cc8ecac41f0d1eb61525ed709ec77cc0bbd
qighliu29 <qighliu29@gmail.com>
2017-05-16 00:11:00 +0800

    fix database issue

--- docker/create-agb-db.sh
+++ docker/create-agb-db.sh
@@ -3,7 +3,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER agb WITH ENCRYPTED PASSWORD 'test_password';
+    CREATE USER agb WITH ENCRYPTED PASSWORD 'agbpassword';
     CREATE DATABASE agb;
     GRANT ALL PRIVILEGES ON DATABASE agb TO agb;
 EOSQL
--- docker/init-agb-db.sh
+++ docker/init-agb-db.sh
@@ -4,24 +4,24 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname agb <<-EOSQL
     CREATE TYPE img_format AS ENUM ('jpeg', 'png', 'gif');
-    CREATE TYPE hot_reason AS ENUM ('query', 'match')
+    CREATE TYPE hot_reason AS ENUM ('query', 'match');
     CREATE TABLE gif
         (id uuid PRIMARY KEY, 
-        url varchar[32], 
-        characteristics_init nvarchar(32)[], 
+        url varchar(128), 
+        characteristics_init varchar(32)[], 
         characteristics integer[], 
         match uuid[], 
         img_size integer, 
         img_type img_format,
         img_hash bytea UNIQUE,
-        source varchar[32], 
+        source varchar(32), 
         create_at timestamp, 
         last_update timestamp);
     CREATE TABLE submit_match
         (id uuid PRIMARY KEY,
         home uuid,
         away uuid,
-        submitter varchar[32],
+        submitter varchar(32),
         submit_at timestamp);
     CREATE TABLE gif_hot
         (id uuid PRIMARY KEY,
@@ -31,6 +31,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname agb <<-EOSQL
         match_hit int);
     CREATE TABLE tag
         (id serial PRIMARY KEY,
-        content nvarchar(32)
-        exp nvarchar(8));
+        content varchar(32),
+        exp int);
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO agb;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO agb;
 EOSQL
